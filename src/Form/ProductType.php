6e3db1f6ac481b7a18dde227fae851862727a4c5
Alexane-trubert <trubert.alexane@orange.fr>
2023-05-27 22:32:30 +0200

    On avance dans l'apprentissage du form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,13 +4,19 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Form\DataTransformer\CentimesTransformer;
+use App\Form\Type\PriceType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -34,7 +40,8 @@ class ProductType extends AbstractType
                 'label' => 'Prix du produit',
                 'attr' => [
                     'placeholder' => 'Ex: 19.99',
-                ]
+                ],
+                'divisor' => 100,
             ])
             ->add('picture', UrlType::class, [
                 'label' => 'Image du produit',
@@ -48,8 +55,32 @@ class ProductType extends AbstractType
                     return strtoupper($category->getName());
                 },
                 'placeholder' => 'Choisir une catégorie',
-            ])
-        ;
+            ]);
+
+//        $builder->get('price')->addModelTransformer(new CentimesTransformer());
+
+/*        $builder->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) {
+                $product = $event->getData();
+
+                if ($product->getPrice() !== null) {
+                    $product->setPrice($product->getPrice() * 100);
+                }
+            }
+        );
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $product = $event->getData();
+
+                if ($product->getPrice() !== null) {
+                    $product->setPrice($product->getPrice() / 100);
+                }
+            }
+        );*/
     }
 
     public function configureOptions(OptionsResolver $resolver): void
